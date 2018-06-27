29a213638dbd9b02473ad2deb274c6b2a10e9ba1
Luis Hernández <ldhdv95@gmail.com>
2018-06-26 19:11:04 -0500

    Create and list all posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,25 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.select(:description).order(created_at: :desc).limit(10) #Esta variable es para usarla en la vista
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save 
+      redirect_to posts_path, notice: 'The post was successfully created'
+    else
+      render :new
+    end
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:description, :location)
+    end
+end
+
+
