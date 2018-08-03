b050d98fef57445c312b039cbd5cefb081b4fefa
Luis Hernández <ldhdv95@gmail.com>
2018-08-02 19:30:34 -0500

    Install RSpec

--- test/mailers/previews/post_mailer_preview.rb
+++
@@ -1,9 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/post_mailer
-class PostMailerPreview < ActionMailer::Preview
-
-  # Preview this email at http://localhost:3000/rails/mailers/post_mailer/congrats
-  def congrats
-    PostMailer.congrats
-  end
-
-end
